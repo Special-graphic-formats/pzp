5341aded54b47aecf68d7ec2082ecd7443d8c016
Ammar Qammaz <ammarkov@gmail.com>
2025-02-01 12:38:37 +0200

    add template

---
+++ scripts/debug.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+
+#Dependency :
+#sudo apt install libzstd-dev
+
+rm  sample.ppm.zst sampleRecode.ppm depth16Recode.ppm segmentRecode.ppm 
+
+gcc zstdInput.c  -D_GNU_SOURCE -O0 -g3 -fno-omit-frame-pointer -Wstrict-overflow  -fPIE -fPIC -lzstd -lm -o zstdInput 
+
+valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --track-origins=yes --num-callers=20 --track-fds=yes ./zstdInput compress sample.ppm sample.pzm $@ 2>error1.txt
+valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --track-origins=yes --num-callers=20 --track-fds=yes ./zstdInput decompress sample.pzm sampleRecode.ppm  $@ 2>error2.txt
+ 
+
+valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --track-origins=yes --num-callers=20 --track-fds=yes ./zstdInput compress depth16.pnm depth16.pzm $@ 2>error3.txt
+valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --track-origins=yes --num-callers=20 --track-fds=yes ./zstdInput decompress depth16.pzm depth16Recode.ppm  $@ 2>error4.txt
+
+
+valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --track-origins=yes --num-callers=20 --track-fds=yes ./zstdInput compress segment.ppm segment.pzm $@ 2>error5.txt
+valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --track-origins=yes --num-callers=20 --track-fds=yes ./zstdInput decompress segment.pzm segmentRecode.ppm  $@ 2>error6.txt
+
+exit 0
---
+++ scripts/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+
+#Dependency :
+#sudo apt install libzstd-dev
+
+
+mkdir output
+
+
+gcc zstdInput.c -lzstd -lm -o zstdInput 
+
+
+./pzp compress samples/sample.ppm output/sample.pzp
+./pzp decompress output/sample.pzp output/sampleRecode.ppm 
+
+./pzp compress samples/depth16.pnm output/depth16.pzp
+./pzp decompress output/depth16.pzp output/depth16Recode.ppm 
+
+./pzp compress samples/rgb8.pnm output/rgb8.pzp
+./pzp decompress output/rgb8.pzp output/rgb8Recode.ppm 
+
+./pzp compress samples/segment.ppm output/segment.pzp
+./pzp decompress output/segment.pzp output/segmentRecode.ppm 
+
+
+exit 0
